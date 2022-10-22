f837d58f0fbfa3e2eaea78289df8b66c1b2d346f
nahidnfr123 <nahidferdous240@gmail.com>
2022-10-22 11:16:04 +0600

    !

--- mv-git.py
+++ mv-git.py
@@ -5,7 +5,7 @@ import subprocess
 baseDir = "/home/nahid/Desktop/Projects/"
 source = "NfrExamination"
 sourcePath = baseDir + source
-projects = ["allexambd", "biddabari-web", "biologykillers", "canvas-ict", "coursecab", "englishmojapb", "marchforwardbd-new", "medilogy", "p2a", "studyplex", "tutoracademia"]
+projects = ["allexambd", "biddabari-web", "biologykillers", "canvas-ict", "coursecab", "englishmojapb", "marchforwardbd-new", "medilogy", "p2a", "studyplex", "tutoracademia", "mediaim"]
 destination = '/src/components/'
 
 
